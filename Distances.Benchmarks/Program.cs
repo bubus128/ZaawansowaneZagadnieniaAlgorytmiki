4ee8d259c194bc3faead564e524600719d1dd48b
Krzysztof Zajączkowski <Krzysztof.m.zajaczkowski@gmail.com>
2023-11-11 16:21:39 +0100

    Add benchmarks #1 (#2)
    * Add Rider folders to .gitignore #1
    
    * Extract functions to their own class #1
    
    * Add benchmarks #1
    
    * Add benchmark workflow #1
    
    * Reduce number of words/sentences to compare to 20 #1
    
    * Remove MemoryDiagnoser #1
    
    * Add static data #1
    
    * Run benchmark workflow only on master #1
    
    * Add better comments #1

---
+++ Distances.Benchmarks/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Security.Cryptography;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using Bogus;
+using Distances;
+
+public class DistancesBenchmark
+{
+    [ParamsSource((nameof(Words)))] 
+    public (string, string) BothWords { get; set; }
+    public static (string, string)[] Words => new[] { ("OUTLIER", "OUTLAW") };
+    
+    [ParamsSource((nameof(Sentences)))]
+    public (string, string) BothSentences { get; set; }
+    public static (string, string)[] Sentences => new[] { ("WIDOCZNOSC NA DRODZE DOBRA", "WIDOCZNOSC DOBRA") };
+    
+    [Benchmark]
+    public int LevensteinWords() => Functions.Levenstein(BothWords.Item1, BothWords.Item2);
+    
+    [Benchmark]
+    public int LevensteinSentences() => Functions.Levenstein(BothSentences.Item1, BothSentences.Item2);
+    
+    [Benchmark]
+    public int LCSWords() => Functions.LCS(BothWords.Item1, BothWords.Item2);
+    
+    [Benchmark]
+    public int LCSSentences() => Functions.LCS(BothSentences.Item1, BothSentences.Item2);
+}
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        BenchmarkRunner.Run<DistancesBenchmark>();
+    }
+}
---
+++ Distances/Functions.cs
@@ -0,0 +1,69 @@
+﻿namespace Distances;
+
+public static class Functions
+{
+    /// <summary>
+    /// Calculate the Levenstein distance between given strings.
+    /// </summary>
+    /// <param name="input1">First string to compare.</param>
+    /// <param name="input2">Second string to compare.</param>
+    /// <returns>The Levenstein distance between given strings.</returns>
+    public static int Levenstein(string input1, string input2)
+    {
+        // Create an array
+        var rowsCount = input1.Length + 1;
+        var colsCount = input2.Length + 1;
+        var d = new int[rowsCount, colsCount];
+
+        // Assign indexes into first row and column
+        Enumerable.Range(0, colsCount).ToList().ForEach(i => d[0, i] = i);
+        Enumerable.Range(0, rowsCount).ToList().ForEach(i => d[i, 0] = i);
+
+        // Calculate the distance
+        for (var i = 1; i < rowsCount; ++i)
+        {
+            for (var j = 1; j < colsCount; ++j)
+            {
+                // Check if any operation is needed
+                var cost = input1[i - 1] == input2[j - 1] ? 0 : 1;
+                // Get the lowest cost of operation
+                d[i, j] = new int[]
+                {
+                    d[i - 1, j] + 1, // deletion
+                    d[i, j - 1] + 1, // insertion
+                    d[i - 1, j - 1] + cost // substitution
+                }.Min();
+            }
+        }
+
+        return d[rowsCount - 1, colsCount - 1];
+    }
+
+    /// <summary>
+    /// Calculate the length of the longest common subsequence.
+    /// </summary>
+    /// <param name="input1">First string to compare.</param>
+    /// <param name="input2">Second string to compare.</param>
+    /// <returns>Length of the longest common subsequence.</returns>
+    // ReSharper disable once InconsistentNaming
+    public static int LCS(string input1, string input2)
+    {
+        // Create an array
+        var rowsCount = input1.Length + 1;
+        var colsCount = input2.Length + 1;
+        var d = new int[rowsCount, colsCount];
+
+        for (var i = 1; i < rowsCount; ++i)
+        {
+            for (var j = 1; j < colsCount; ++j)
+            {
+                // Check if characters are the same
+                d[i, j] = input1[i - 1] == input2[j - 1]
+                    ? d[i - 1, j - 1] + 1 // If yes increment the length
+                    : Math.Max(d[i, j - 1], d[i - 1, j]); // If no get the longer sequence.
+            }
+        }
+
+        return d[rowsCount - 1, colsCount - 1];
+    }
+}
