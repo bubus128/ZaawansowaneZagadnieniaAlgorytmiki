5a331a0cae0f4061efb18d765890f2c50a545f40
bubus128 <j.swislocki@outlook.com>
2023-11-26 09:50:01 +0100

    Add Boyer Moore algorithm

---
+++ Substrings/Substrings.cs
@@ -0,0 +1,32 @@
+﻿namespace Substrings
+{
+    public class Substrings
+    {
+        private static string GetInputUntilValid(string inputName)
+        {
+            while (true)
+            {
+                Console.Write($"{inputName}:");
+                var input = Console.ReadLine();
+
+                if (!string.IsNullOrWhiteSpace(input))
+                {
+                    return input;
+                }
+            }
+        }
+
+        public static void Main()
+        {
+            // Get inputs
+            var text = GetInputUntilValid("Text");
+            var substring = GetInputUntilValid("Substring");
+
+            // Find the substring using Boyer Moore algorithm
+            int boyerMooreIndex = Functions.BoyerMoore(text, substring);
+            Console.WriteLine(boyerMooreIndex > -1 ?
+                $"Substring found at index: {boyerMooreIndex}" :
+                "Substring not found");
+        }
+    }
+}
