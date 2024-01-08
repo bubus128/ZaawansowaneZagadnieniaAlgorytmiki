b9e854af41bb02c368ea3acf1712932e42fca5d5
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2024-01-08 23:13:39 +0100

    #9 Add printing data

---
+++ Distances/TableWriter.cs
@@ -0,0 +1,54 @@
+﻿using System.Text;
+
+namespace Distances;
+
+public static class TableWriter
+{
+    private static void PrintHorizontalTableLine(StringBuilder sb, int width, Action<string> lineWriter)
+    {
+        sb.Clear()
+            .Append(new string('-', width + 1));
+        lineWriter(sb.ToString());
+        sb.Clear();
+    }
+
+    public static IEnumerable<int> EnumerateMultidimensionalTable(int[,] table)
+    {
+        foreach (var i in table)
+        {
+            yield return i;
+        }
+    }
+
+    public static void Print(int[,] table, Action<string> lineWriter, int? currentRow = null, int? currentCol = null, Action? styleRow = null, Action? resetStyleRow = null)
+    {
+        var numberOfCharacters = EnumerateMultidimensionalTable(table).Max().ToString().Length;
+
+        var sb = new StringBuilder();
+        var rowsLength = table.GetLength(0);
+        var columnsLength = table.GetLength(1);
+
+        PrintHorizontalTableLine(sb, columnsLength * (numberOfCharacters + 1), lineWriter);
+        
+        for (var row = 0; row < rowsLength; ++row)
+        {
+            if (currentRow != null && styleRow != null && resetStyleRow != null && currentRow == row)
+            {
+                styleRow();
+            }
+            
+            sb.Clear().Append(' ');
+            for (var col = 0; col < columnsLength; ++col)
+            {
+                sb.Append($"{table[row, col].ToString().PadLeft(numberOfCharacters)} ");
+            }
+
+            lineWriter(sb.ToString());
+
+            resetStyleRow?.Invoke();
+        }
+        
+        PrintHorizontalTableLine(sb, columnsLength * (numberOfCharacters + 1), lineWriter);
+        lineWriter("");
+    }
+}
